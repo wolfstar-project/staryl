00b6346b818f8921db55dbf4039765e1c31f8a01
RedStar <redstar071@proton.me>
2026-04-03 13:21:20 +0200

    feat(schedules): register ScheduleHandlerStore in the container

--- src/lib/structures/ScheduleHandler.ts
+++ src/lib/structures/ScheduleHandler.ts
@@ -1,36 +1,38 @@
-import type { BaseScheduler } from '#lib/schedules/BaseScheduler';
-import type { Awaitable } from '@sapphire/utilities';
-import { Piece } from '@skyra/http-framework';
+import type { BaseScheduler } from "#lib/schedules/BaseScheduler";
+import type { Awaitable } from "@sapphire/utilities";
+import { Piece } from "@skyra/http-framework";
 
 export abstract class ScheduleHandler<T extends BaseScheduler.Value = BaseScheduler.Value> extends Piece {
-	public abstract run(data: BaseScheduler.AddId<T>): Awaitable<ScheduleHandler.Result>;
+  public abstract run(data: BaseScheduler.AddId<T>): Awaitable<ScheduleHandler.Result>;
 
-	/**
-	 * @param ms The duration in milliseconds.
-	 */
-	protected delay(ms: number): ScheduleHandler.Reschedule {
-		return { type: ScheduleHandler.Type.Reschedule, value: Date.now() + ms };
-	}
+  /**
+   * #param ms The duration in milliseconds.
+   */
+  protected delay(ms: number): ScheduleHandler.Reschedule {
+    return { type: ScheduleHandler.Type.Reschedule, value: Date.now() + ms };
+  }
 
-	protected finish(): ScheduleHandler.Finish {
-		return { type: ScheduleHandler.Type.Finish };
-	}
+  protected finish(): ScheduleHandler.Finish {
+    return { type: ScheduleHandler.Type.Finish };
+  }
 }
 
+// eslint-disable-next-line ts/no-namespace
 export namespace ScheduleHandler {
-	export const enum Type {
-		Reschedule,
-		Finish
-	}
+  // eslint-disable-next-line no-restricted-syntax
+  export const enum Type {
+    Reschedule,
+    Finish,
+  }
 
-	export type Result = Finish | Reschedule;
+  export type Result = Finish | Reschedule;
 
-	export interface Finish {
-		type: Type.Finish;
-	}
+  export interface Finish {
+    type: Type.Finish;
+  }
 
-	export interface Reschedule {
-		type: Type.Reschedule;
-		value: number;
-	}
+  export interface Reschedule {
+    type: Type.Reschedule;
+    value: number;
+  }
 }
