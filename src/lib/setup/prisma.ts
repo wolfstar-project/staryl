c796202f933af9e2118970a713256332f2fc4d7f
RedStar <redstar071@proton.me>
2026-04-04 14:02:58 +0200

    refactor(prisma): simplify database client initialization and remove unused function

--- src/lib/setup/prisma.ts
+++ src/lib/setup/prisma.ts
@@ -2,19 +2,10 @@ import { PrismaClient } from "#generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { container } from "@skyra/http-framework";
 
-interface GetDbParams {
-  connectionString: string;
-}
-
-function getDb({ connectionString }: GetDbParams) {
-  const pool = new PrismaPg({ connectionString });
-
-  const prisma = new PrismaClient({ adapter: pool });
-
-  return prisma;
-}
-const connectionString = `${process.env.DATABASE_URL}`;
-const prisma = getDb({ connectionString });
+const adapter = new PrismaPg({
+  connectionString: `${process.env.DATABASE_URL}`,
+});
+const prisma = new PrismaClient({ adapter });
 container.prisma = prisma;
 
 declare module "@sapphire/pieces" {
