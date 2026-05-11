bb8751400bc58ba9d249bc43deab6afbf27400fa
RedStar <redstar071@proton.me>
2026-05-11 17:53:01 +0200

    fix: update setRepository to use the project name instead of URL

--- src/lib/setup/all.ts
+++ src/lib/setup/all.ts
@@ -8,7 +8,7 @@ import {
 import "@skyra/shared-http-pieces/register";
 
 envRun(new URL("../../../src/.env", import.meta.url));
-setRepository("https://github.com/wolstar-project/starly");
+setRepository("starly");
 setInvite("12345678906342567", "0");
 initializeSentry();
 
