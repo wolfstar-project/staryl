905dac02c9a76ef9ad2b66c514bec372da8fe97e
RedStar <redstar071@proton.me>
2026-05-11 13:59:05 +0200

    style: format taze.config.ts for consistency

--- taze.config.ts
+++ taze.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from "taze";
 
 export default defineConfig({
   write: true,
-  // Run `npm install` or `yarn install` right after bumping
+  // run `npm install` or `yarn install` right after bumping
   install: true,
   update: true,
   recursive: true,
   includeLocked: true,
   interactive: true,
-  packageMode: {
-    "@prisma/client": "ignore",
-    "@prisma/adapter-pg": "ignore",
-    "prisma-json-types-generator": "ignore",
-    prisma: "ignore",
-  },
   depFields: {
     overrides: false,
   },
