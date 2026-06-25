af1f7a7800b03afcb01b45563c6431f85e1297e9
RedStar <redstar071@proton.me>
2026-06-25 13:28:57 +0200

    chore: update dependencies, improve environment setup, and refine .gitignore (#45)

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
-import { config } from "dotenv";
+import { setup as envRun } from "@wolfstar/env-utilities";
 import { defineConfig, env } from "prisma/config";
 
-config({ path: ["src/.env.local", "src/.env"] });
+envRun(new URL("./src/.env", import.meta.url));
 export default defineConfig({
 	schema: "prisma/schema.prisma",
 	migrations: {
